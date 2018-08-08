c4833b54571b6dfbe08a76b9d04b037966bb3a1d
reagankg <reagankganancial@gmail.com>
2018-08-08 21:54:53 +0800

    3rd

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  mongoURI: 'mongodb://reagankg:shainashmone3@ds163781.mlab.com:63781/myconnector',
   secretOrKey: 'secret'
 };
